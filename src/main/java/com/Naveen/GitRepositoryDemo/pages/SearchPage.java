d0dd43851d779aa30ced3d7560eec21e89857e8a
jalda123 <sateesh.jalda2020@gmail.com>
2018-06-30 17:20:08 +0530

    new code

---
+++ src/main/java/com/Naveen/GitRepositoryDemo/pages/SearchPage.java
@@ -0,0 +1,13 @@
+package com.Naveen.GitRepositoryDemo.pages;
+
+import org.testng.annotations.Test;
+
+public class SearchPage {
+	
+	@Test
+	
+	public void search() {
+		System.out.println("Search paage");
+	}
+
+}
